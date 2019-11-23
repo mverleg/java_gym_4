d83c15509c5326e1be8aa37cfcbf1f2dd1099c2b
Mark <mverleg.noreply@gmail.com>
2019-11-23 13:40:35 +0100

    Include heavier performance test

--- src/test/java/javagym/MarkSolutionTest.java
+++ src/test/java/javagym/MarkSolutionTest.java
@@ -1,39 +1,15 @@
 package javagym;
 
-import javax.annotation.Nonnull;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
-import noedit.Cell;
 import noedit.Maze;
 import noedit.MazeGenerator;
-import noedit.Path;
 import noedit.Position;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static javagym.SolutionTest.checkMazeSolution;
 
 public class MarkSolutionTest {
-
-    private double checkMazeSolution(
-            @Nonnull Maze maze,
-            @Nonnull Position initialPosition
-    ) {
-        Solution solution = new Solution();
-        long t0 = System.nanoTime();
-        Path path = solution.solve(maze, initialPosition);
-        long duration = System.nanoTime() - t0;
-        assertEquals(path.first(), initialPosition,
-                "The solution path does not start at the initial position");
-        assertTrue(path.isPhysical(),
-                "The solution contains impossible moves, like jumping multiple squares, or going back in time.");
-        assertEquals(Cell.Exit, maze.get(path.last()),
-                "The solution does not solve the maze (it does not end at an exit).");
-        assertTrue(path.isSolution(maze),
-                "The solution is not valid for the maze — it may cross walls or leave the maze area.");
-        return duration / 1e6;
-    }
 
     @Test
     void testPerformanceMark() {
