e9497ee6b03f33b7e5ebb61384e9bd866de075a5
Mark <mverleg.noreply@gmail.com>
2019-11-23 13:01:41 +0100

    Extract parameters and add a test for many exits

--- src/main/java/javagym/Parameters.java
+++ src/main/java/javagym/Parameters.java
@@ -2,7 +2,7 @@ package javagym;
 
 public interface Parameters {
 
-	int EXIT_TRACK_LIMIT = 200;
+	int EXIT_TRACK_LIMIT = 1000000;
 	int TUNNEL_FOLLOW_LENGTH = 16;
 	int CHOKEPOINT_LAYER_LIMIT = 3;
 	int TIME_WEIGHT = 80;
