0b3037cefd4b4d2953f176ef7d42c4c22a0dae75
Mark <mverleg.noreply@gmail.com>
2019-11-23 11:31:31 +0100

    Attempt to find chokepoints

--- src/main/java/javagym/Solution.java
+++ src/main/java/javagym/Solution.java
@@ -1,10 +1,13 @@
 package javagym;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.Validate;
 
 import noedit.Maze;
@@ -20,7 +23,7 @@ public class Solution {
 
     //TODO @mark: focus more on time holes
     //TODO @mark: flamegraph
-    //TODO @mark: don't follow tunnels too long
+    //TODO @mark: if there are too many target points, it's better to ignore them alltogether, rather than take the first few
 
     @Nonnull
     @CheckReturnValue
@@ -29,15 +32,18 @@ public class Solution {
                 "Started inside a wall; this should never happen");
 
         // First find the exits.
-        Position[] exits = findExits(maze, initialPosition, 16);
-        Validate.isTrue(exits.length > 0, "No exits, IT'S A TRAP!");
+        Position[] targets = ArrayUtils.addAll(
+                findExits(maze, initialPosition, 16),
+                findTimeChokePoints(maze, 3)
+        );
+        Validate.isTrue(targets.length > 0, "No exits, IT'S A TRAP!");
 
         //TODO @mark: TEMPORARY! REMOVE THIS!
         // System.out.println(maze.asStringAll());
         // System.out.println(costGridAsText(maze, exits));
 
         // Create a priority queue of items to process.
-        Queue queue = new Queue(exits);
+        Queue queue = new Queue(targets);
 
         // And create a grid of visited cells.
         VisitGrid grid = new VisitGrid(maze);
@@ -55,13 +61,15 @@ public class Solution {
         while (queue.isNotEmpty()) {
 
             PathBuilder path = queue.head();
-            assert path != null: "No more items to explore, even though no exit has been found";
+            assert path != null : "No more items to explore, even though no exit has been found";
             // System.out.println("taking " + path.latest() + " cost " + smallestDist(path.latest(), exits));  //TODO @mark: TEMPORARY! REMOVE THIS!
 
             // Find which neighbours can be explored.
             // If there is only one (tunnel), follow it.
             Position[] neighbours = findExplorable(path.latest(), maze, grid);
-            while (neighbours.length == 1) {
+            int tunnelSteps = 0;
+            //TODO @mark: tune this tunnelSteps limit?
+            while (tunnelSteps < 15 && neighbours.length == 1) {
                 Position neighbour = neighbours[0];
                 // Pay attention that anything added to path must be 1) checked for exit and 2) marked as visited.
                 path.add(neighbour);
@@ -72,10 +80,11 @@ public class Solution {
                     return path.build();
                 }
                 grid.mark(neighbour);
+                tunnelSteps += 1;
                 neighbours = findExplorable(path.latest(), maze, grid);
             }
 
-            // Add all the options (either a split, or a dead end).
+            // Add all the options (either a split, or a dead end, or too long tunnel).
             for (Position neighbour : neighbours) {
                 PathBuilder newPath = path.clone();
                 // Pay attention that anything added to path must be 1) checked for exit and 2) marked as visited.
@@ -111,10 +120,48 @@ public class Solution {
     }
 
     /**
+     * Find layers with few accessible open spaces, so that they can be rargeted.
+     */
+    @Nonnull
+    @CheckReturnValue
+    @SuppressWarnings("SameParameterValue")
+    private Position[] findTimeChokePoints(Maze maze, int maxPerLayer) {
+        if (maze.duration <= 2) {
+            return new Position[0];
+        }
+        @SuppressWarnings("unchecked")
+        List<Position>[] layersPassages = new ArrayList[maze.duration - 2];
+        for (int t = 1; t < maze.duration - 1; t++) {
+            ArrayList<Position> currentLayerPackages = new ArrayList<>();
+            for (int x = 0; x < maze.width; x++) {
+                for (int y = 0; y < maze.height; y++) {
+                    boolean prevOpen = maze.getOrElse(t - 1, x, y, Wall) != Wall;
+                    boolean curOpen = maze.getOrElse(t, x, y, Wall) != Wall;
+                    boolean nextOpen = maze.getOrElse(t + 1, x, y, Wall) != Wall;
+                    boolean isPassage = (prevOpen && curOpen) || (curOpen && nextOpen);
+                    if (isPassage) {
+                        currentLayerPackages.add(Position.at(t, x, y));
+                    }
+                }
+                if (currentLayerPackages.size() > maxPerLayer) {
+                    break;
+                }
+            }
+            Validate.isTrue(!currentLayerPackages.isEmpty());
+            layersPassages[t - 1] = currentLayerPackages;
+        }
+        return Arrays.stream(layersPassages)
+                .filter(passages -> passages.size() <= maxPerLayer)
+                .flatMap(passages -> passages.stream())
+                .toArray(Position[]::new);
+    }
+
+    /**
      * Create an array of exits, from nearest to furthest from initial position.
      */
     @Nonnull
     @CheckReturnValue
+    @SuppressWarnings("SameParameterValue")
     private Position[] findExits(@Nonnull Maze maze, @Nonnull Position initialPosition, int maxCount) {
         return maze.stream()
                 .filter(cell -> cell.getRight() == Exit)
